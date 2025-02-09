d01988bfe424823b5850ba805d17bf81ff023da3
Shouryya <shouryyamanna06@gmail.com>
2025-02-09 08:31:03 +0530

    Server displays all users

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+
+const PORT = 8000;
+
+app.get("/users",(req,res)=>{
+    const html =`
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+
+
+app.get("/api/users",(req,res)=>{
+    res.json(users);
+});
+
+app.listen(PORT,()=>{
+    console.log("Server has started...")
+});
