8c9d2d59fc74c0b9dda3566cb364dd7002d1c5f8
Shouryya <shouryyamanna06@gmail.com>
2025-02-09 22:28:06 +0530

    third commit

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ app.get("/api/users",(req,res)=>{
     res.json(users);
 });
 
+app.route("/api/users/:id")
+
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id===id)
+    return res.json(user);
+})
+.patch((req,res)=>{
+    return res.json({status:"pending"});
+})
+.delete((req,res)=>{
+    return res.json({status:"pending"});
+});
+
+
+app.post("/api/users",(req,res)=>{
+    return res.json({status:"pending"});
+});
+
+
 app.listen(PORT,()=>{
     console.log("Server has started...")
 });
