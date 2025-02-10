05033b793594e22a073d5fcfa5a04c049657e2cb
Shouryya <shouryyamanna06@gmail.com>
2025-02-10 23:32:57 +0530

    minor patches

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
 const app = express();
-
 const PORT = 8000;
 
 app.get("/users",(req,res)=>{
@@ -17,9 +16,11 @@ app.get("/users",(req,res)=>{
 app.get("/api/users",(req,res)=>{
     res.json(users);
 });
+app.get("/",(req,res)=>{
+    res.send("Hello from Home Page");
+});
 
 app.route("/api/users/:id")
-
 .get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user)=>user.id===id)
