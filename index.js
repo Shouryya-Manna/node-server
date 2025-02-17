93a9f6e42a3dc1d62b3aa60b6f77bc13d7dc73c6
Shouryya <shouryyamanna06@gmail.com>
2025-02-17 20:30:07 +0530

    Implemented Middleware

--- index.js
+++ index.js
@@ -5,53 +5,61 @@ const PORT = 8000;
 const fs = require("fs");
 
 //middleware
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/users",(req,res)=>{
-    const html =`
+app.use((req, res, next) => {
+  console.log("Hello from middleware 1");
+  req.myUserName = "shouryyamanna.dev";
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Hello from middleware 2", req.myUserName);
+  next();
+});
+
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
-    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
     </ul>
     `;
-    res.send(html);
+  res.send(html);
 });
 
-
-app.get("/api/users",(req,res)=>{
-    res.json(users);
+app.get("/api/users", (req, res) => {
+  res.json(users);
 });
-app.get("/",(req,res)=>{
-    res.send("Hello from Home Page");
+app.get("/", (req, res) => {
+  res.send("Hello from Home Page");
 });
 
-app.route("/api/users/:id")
-.get((req,res)=>{
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find((user)=>user.id===id)
+    const user = users.find((user) => user.id === id);
     return res.json(user);
-})
-.patch((req,res)=>{
+  })
+  .patch((req, res) => {
     //edit the user details
-    return res.json({status:"pending"});
-})
-.delete((req,res)=>{
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
     //delete user
-    return res.json({status:"pending"});
+    return res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  //create user
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
+  console.log("Body", body);
 });
 
-
-app.post("/api/users",(req,res)=>{
-    //create user
-    const body = req.body;
-    users.push({...body, id:users.length+1});
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-        return res.json({status:"success",id:users.length});
-    });
-    console.log("Body",body);
-    
-});
-
-
-app.listen(PORT,()=>{
-    console.log("Server has started...")
+app.listen(PORT, () => {
+  console.log("Server has started...");
 });
